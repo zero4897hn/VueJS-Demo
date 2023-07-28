f6665cece669c1473235a072e44ef17fa7b637da
zero4897hn <zero4897hncool@gmail.com>
2023-07-28 12:49:08 +0900

    Adding computed

---
+++ src/components/CurrencyHandler.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul v-for="(currency, index) in formatCurrency" v-bind:key="index">{{ currency }}</ul>
+</template>
+
+<script>
+export default {
+    name: "CounterHandler",
+    computed: {
+        formatCurrency() {
+            const formatedCurrencies = [];
+            this.amounts.forEach(element => {
+                formatedCurrencies.push(element.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'))
+            })
+            return formatedCurrencies
+        }
+    },
+    data() {
+        return {
+            amounts: [1000, 2500, 800]
+        }
+    },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import store from './store'
 import { createRouter, createWebHistory } from 'vue-router'
 import CounterHandler from './components/CounterHandler'
+import CurrencyHandler from './components/CurrencyHandler'
 import HomePage from './components/HomePage'
 
 const routes = [
     { path: '/', component: HomePage },
-    { path: '/counter', component: CounterHandler }
+    { path: '/counter', component: CounterHandler },
+    { path: '/currency', component: CurrencyHandler }
 ]
 
 const router = createRouter({
